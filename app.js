4453a4215f57b5a9b16264b9ecf7c5ae8d7005e2
JokoPrasetyoUtomo <joko345@users.noreply.github.com>
2024-09-23 18:28:35 +0700

    new

--- app.js
+++ app.js
@@ -27,3 +27,21 @@ const intFromPX = parseInt(px);//parses membaca perkata jika bukan number maka k
 
 console.log(intFromCM); // output: 20
 console.log(intFromPX); // output: 64
+
+// AND
+console.log(true && true); // true
+console.log(false && true); // false
+console.log(true && false); // false
+console.log(false && false); // false
+console.log((5 === 5) && (3 < 5)); // true
+ 
+// OR
+console.log(true || true); // true
+console.log(false || true); // true
+console.log(true || false); // true
+console.log(false || false); // false
+console.log((5 === 5) || (3 > 5)); // true
+ 
+// NOT
+console.log(!true); // false
+console.log(!false); // true
