f2d9ddc2f0dc59ca5d8909b21a8ed5bf349a3a05
JokoPrasetyoUtomo <joko345@users.noreply.github.com>
2024-10-22 12:57:15 +0700

    new

--- app.js
+++ app.js
@@ -1,32 +1,3 @@
-console.log(process.platform);
-const age = 10;
-const name = 'Dicoding';
-console.log(`Aku ${name}, umurku ${age} tahun.`);
-let username = 'Dicoding';
-
-console.log('Sebelum diubah:', username); // output: Sebelum diubah: Dicoding
-
-username = 'dicodingacademy';
-
-console.log('Setelah diubah:', username); // output: Setelah diubah: dicodingacademy
-
-const number = 123;
-const boolean = true;
-
-const strNumber = String(number);//konversi number ke str
-const strBoolean = boolean.toString();
-
-console.log(strNumber); // output: "123"
-console.log(strBoolean); // output: "true"
-
-const cm = '20cm';
-const px = '64px';
-
-const intFromCM = parseInt(cm);
-const intFromPX = parseInt(px);//parses membaca perkata jika bukan number maka konversi berhenti
-
-console.log(intFromCM); // output: 20
-console.log(intFromPX); // output: 64
 
 // AND
 console.log(true && true); // true
@@ -45,3 +16,13 @@ console.log((5 === 5) || (3 > 5)); // true
 // NOT
 console.log(!true); // false
 console.log(!false); // true
+
+//variable dynamic js
+let myNum = 0;
+myNum = 1;
+
+console.log(myNum) // 1
+
+myNum = true;
+
+console.log(myNum) // true
---
+++ greet.js
@@ -0,0 +1,5 @@
+var myName = 'TypeScript';
+function greet(name) {
+    console.log("Hello, ".concat(name, ". My name is ").concat(myName));
+}
+greet('JavaScript');
